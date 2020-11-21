4b33aa9e4b5310220f8aac2b486c65f9a94bb88a
maksimus <gorlovmaks23@gmail.com>
2020-11-21 19:45:47 +0600

    Добавила модель для создания и хранения категорий в базе данных

---
+++ app/categories.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const Category = require("../models/Category");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    try {
+        const categories = await Category.find();
+        return res.send(categories);
+    } catch (e) {
+        return res.status(422).send(e);
+    }
+});
+
+module.exports = router;
